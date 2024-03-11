d7a44d848bb495960416dd8d03ca0bf1cb9eb658
tiagonic <tiagonic@gmail.com>
2024-03-11 03:45:00 -0300

    Código a ser inserido no livro Sociedade do Cristal

--- code/code.c
+++ code/code.c
@@ -2,25 +2,25 @@
 #include <string.h>
 
 void printTabelaASCII() {
-    for (int i = 0; i < 256; i++) {
-        printf("%08b=%c\n", i, i);
+    for (int i = 33; i < 127; i++) {
+        printf("%3d %08b %c\n", i, i, i);
     }
 }
 
 void exportarTabelaASCII() {
-    char *arq = "ascii.txt";
+    char *arq = "saida/ascii.txt";
     FILE *file = fopen(arq, "w+");
     
-    fprintf(file, "%s;%s\n", "Código", "Caractere");
-    for (int i = 0; i < 256; i++) {
-        fprintf(file, "%08b;%c\n", i, i);
+    fprintf(file, "%s;%s;%s\n", "Decimal","Binário", "Caractere");
+    for (int i = 33; i < 127; i++) {
+        fprintf(file, "%3d;%08b;%c\n", i, i, i);
     }
     fclose(file);
 }
 
 int main(int size, char *args[]) {
     if(size > 1) {
-        char *arq0 = "code.txt", *arq1 = "decode.txt";
+        char *arq0 = "saida/code.txt", *arq1 = "saida/decode.txt";
         FILE *file0 = fopen(arq0, "w+"), *file1 = fopen(arq1, "w+");
         for(int i = 1; i < size; i++) {
             for(int j = 0; j < strlen(args[i]); j++) {
